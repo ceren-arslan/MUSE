c3d93b18e4595007faa0eb621565b4fffee33244
ceren-arslan <cerenarslan194@gmail.com>
2022-06-15 10:57:30 +0200

    frequencies change in steps of half an octave

--- generate_tone_CA.m
+++ generate_tone_CA.m
@@ -4,19 +4,29 @@
 % 14/06/2022
 
 clear all; close all; clc; randn('seed',0); rand('seed',0); fprintf('.\n');
-addpath('C:/Users/ceren/Desktop/RM Cog/piheycdi/stimuli/tones/');
-dataout = 'C:/Users/ceren/Desktop/RM Cog/piheycdi/stimuli/tones/'; 
+
+%office 
+addpath('C:/Users/arslan/Documents/piheycdi/MUSE/tones/wolff_et_all/'); 
+addpath ('C:/Program Files/MATLAB/R2022a/tone_generator');
+dataout = 'C:/Users/arslan/Documents/piheycdi/MUSE/tones/wolff_et_all/'; 
+
+%in laptop
+%addpath('C:/Users/ceren/Desktop/RM Cog/piheycdi/stimuli/tones/');
+%dataout = 'C:/Users/ceren/Desktop/RM Cog/piheycdi/stimuli/tones/'; 
+
 
 %define parameters
 
 fs = 44100;                              % sampling frequency (Hz)
 duration = 200;                          % signal duration (ms)
 amplitude = 1;                          % pure tone amplitude
-frequency_list = [380, 419, 463, 511, 564, 623, 688, 759, 838, 925, 1022, 1128, 1245, 1375]; % pure tone frequency (Hz)
+% frequency_list = [380, 419, 463, 511, 564, 623, 688, 759, 838, 925, 1022, 1128, 1245, 1375]; % pure tone frequency (Hz)
+frequency_list = [270, 381, 540, 763, 1080, 1527, 2159, 3054]; % in steps of half an octave = previous freq*sqrt(2)
 phase = pi/16;                          % pure tone phase (rad/sec)????????
 fade_duration = 20;                     % fade-in and fade-out duration (ms)
 N = floor(duration*1E-3*fs);            % signal length (samples)
 window = @(N)( chebwin(N,100) );        % analysis window function
+fade_window = @(N)( hanning(N).^2 );    % fade-in and fade-out window function handle
 
     % generate a pure tone
     for i = 1:length(frequency_list)
@@ -25,3 +35,16 @@ window = @(N)( chebwin(N,100) );        % analysis window function
         filename = [num2str(frequency_list(i)),'Hz.wav'] ;
         audiowrite([dataout,filename],tone,fs);
     end
+% define parameters specific to generation of the mixture of pure tones
+amplitudes = ones (1);                  % pure tone amplitudes
+phases = [pi/16, pi/16,pi/16,pi/16,pi/16,pi/16,pi/16,pi/16];               % pure tone phase (rad/sec)
+fade_durations = [ 20 20 ];             % fade-in and fade-out durations (ms)
+
+% fade-in and fade-out window function handles
+fade_windows = { @(N)(hanning(N).^2) @(N)(chebwin(N,100)) };
+
+% generate a mixture of pure tones
+tones = tone_generator( fs, duration, amplitudes, frequency_list, phases, fade_durations, fade_windows );
+soundsc(tones, fs, 16)
+filename = 'mask.wav'; 
+audiowrite([dataout,filename],tones,fs);
